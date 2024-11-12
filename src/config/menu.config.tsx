662bdc6c42f9f8a7c54f546e4d93ab19a2b583b1
baraah-berra <baraahalsaidramadan@gmail.com>
2024-11-12 19:03:43 +0300

    update sidebar menu titles and subtitles

--- src/config/menu.config.tsx
+++ src/config/menu.config.tsx
@@ -15,419 +15,59 @@ export const MENU_SIDEBAR: TMenuConfig = [
       }
     ]
   },
+  
   {
-    heading: 'User'
-  },
-  {
-    title: 'Public Profile',
+    title: 'Monitoring',
     icon: 'profile-circle',
     children: [
-      {
-        title: 'Profiles',
-        children: [
-          {
-            title: 'Default',
-            path: '/public-profile/profiles/default'
-          },
-          {
-            title: 'Creator',
-            path: '/public-profile/profiles/creator'
-          },
-          {
-            title: 'Company',
-            path: '/public-profile/profiles/company'
-          },
-          {
-            title: 'NFT',
-            path: '/public-profile/profiles/nft'
-          },
-          {
-            title: 'Blogger',
-            path: '/public-profile/profiles/blogger'
-          },
-          {
-            title: 'CRM',
-            path: '/public-profile/profiles/crm'
-          },
-          {
-            title: 'More',
-            collapse: true,
-            collapseTitle: 'Show less',
-            expandTitle: 'Show 4 more',
-            dropdownProps: {
-              placement: 'right-start'
-            },
-            children: [
-              {
-                title: 'Gamer',
-                path: '/public-profile/profiles/gamer'
-              },
-              {
-                title: 'Feeds',
-                path: '/public-profile/profiles/feeds'
-              },
-              {
-                title: 'Plain',
-                path: '/public-profile/profiles/plain'
-              },
-              {
-                title: 'Modal',
-                path: '/public-profile/profiles/modal'
-              }
-            ]
-          }
-        ]
-      },
-      {
-        title: 'Projects',
-        children: [
-          {
-            title: '3 Columns',
-            path: '/public-profile/projects/3-columns'
-          },
-          {
-            title: '2 Columns',
-            path: '/public-profile/projects/2-columns'
-          }
-        ]
-      },
-      {
-        title: 'Works',
-        path: '/public-profile/works'
-      },
-      {
-        title: 'Teams',
-        path: '/public-profile/teams'
-      },
-      {
-        title: 'Network',
-        path: '/public-profile/network'
-      },
-      {
-        title: 'Activity',
-        path: '/public-profile/activity'
-      },
-      {
-        title: 'More',
-        collapse: true,
-        collapseTitle: 'Show less',
-        expandTitle: 'Show 3 more',
-        dropdownProps: {
-          placement: 'right-start'
-        },
-        children: [
-          {
-            title: 'Campaigns - Card',
-            path: '/public-profile/campaigns/card'
-          },
-          {
-            title: 'Campaigns - List',
-            path: '/public-profile/campaigns/list'
-          },
-          {
-            title: 'Empty',
-            path: '/public-profile/empty'
-          }
-        ]
-      }
+      { },
+
     ]
+    
   },
   {
-    title: 'My Account',
+    title: 'Trips',
     icon: 'setting-2',
     children: [
-      {
-        title: 'Account',
-        children: [
-          {
-            title: 'Get Started',
-            path: '/account/home/get-started'
-          },
-          {
-            title: 'User Profile',
-            path: '/account/home/user-profile'
-          },
-          {
-            title: 'Company Profile',
-            path: '/account/home/company-profile'
-          },
-          {
-            title: 'Settings - With Sidebar',
-            path: '/account/home/settings-sidebar'
-          },
-          {
-            title: 'Settings - Enterprise',
-            path: '/account/home/settings-enterprise'
-          },
-          {
-            title: 'Settings - Plain',
-            path: '/account/home/settings-plain'
-          },
-          {
-            title: 'Settings - Modal',
-            path: '/account/home/settings-modal'
-          }
-        ]
-      },
-      {
-        title: 'Billing',
-        children: [
-          {
-            title: 'Billing - Basic',
-            path: '/account/billing/basic'
-          },
-          {
-            title: 'Billing - Enterprise',
-            path: '/account/billing/enterprise'
-          },
-          {
-            title: 'Plans',
-            path: '/account/billing/plans'
-          },
-          {
-            title: 'Billing History',
-            path: '/account/billing/history'
-          }
-        ]
-      },
-      {
-        title: 'Security',
-        children: [
-          {
-            title: 'Get Started',
-            path: '/account/security/get-started'
-          },
-          {
-            title: 'Security Overview',
-            path: '/account/security/overview'
-          },
-          {
-            title: 'Allowed IP Addresses',
-            path: '/account/security/allowed-ip-addresses'
-          },
-          {
-            title: 'Privacy Settings',
-            path: '/account/security/privacy-settings'
-          },
-          {
-            title: 'Device Management',
-            path: '/account/security/device-management'
-          },
-          {
-            title: 'Backup & Recovery',
-            path: '/account/security/backup-and-recovery'
-          },
-          {
-            title: 'Current Sessions',
-            path: '/account/security/current-sessions'
-          },
-          {
-            title: 'Security Log',
-            path: '/account/security/security-log'
-          }
-        ]
-      },
-      {
-        title: 'Members & Roles',
-        children: [
-          {
-            title: 'Teams Starter',
-            path: '/account/members/team-starter'
-          },
-          {
-            title: 'Teams',
-            path: '/account/members/teams'
-          },
-          {
-            title: 'Team Info',
-            path: '/account/members/team-info'
-          },
-          {
-            title: 'Members Starter',
-            path: '/account/members/members-starter'
-          },
-          {
-            title: 'Team Members',
-            path: '/account/members/team-members'
-          },
-          {
-            title: 'Import Members',
-            path: '/account/members/import-members'
-          },
-          {
-            title: 'Roles',
-            path: '/account/members/roles'
-          },
-          {
-            title: 'Permissions - Toggler',
-            path: '/account/members/permissions-toggle'
-          },
-          {
-            title: 'Permissions - Check',
-            path: '/account/members/permissions-check'
-          }
-        ]
-      },
-      {
-        title: 'Integrations',
-        path: '/account/integrations'
-      },
-      {
-        title: 'Notifications',
-        path: '/account/notifications'
-      },
-      {
-        title: 'API Keys',
-        path: '/account/api-keys'
-      },
-      {
-        title: 'More',
-        collapse: true,
-        collapseTitle: 'Show less',
-        expandTitle: 'Show 3 more',
-        dropdownProps: {
-          placement: 'right-start'
-        },
-        children: [
-          {
-            title: 'Appearance',
-            path: '/account/appearance'
-          },
-          {
-            title: 'Invite a Friend',
-            path: '/account/invite-a-friend'
-          },
-          {
-            title: 'Activity',
-            path: '/account/activity'
-          }
-        ]
-      }
+      { },
+
     ]
   },
   {
-    title: 'Network',
+    title: 'Geofence',
     icon: 'users',
     children: [
       {
-        title: 'Get Started',
-        path: '/network/get-started'
+        title: 'Geofence',
+        path: '/geofence/get-started'
       },
       {
-        title: 'User Cards',
-        children: [
-          {
-            title: 'Mini Cards',
-            path: '/network/user-cards/mini-cards'
-          },
-          {
-            title: 'Team Crew',
-            path: '/network/user-cards/team-crew'
-          },
-          {
-            title: 'Author',
-            path: '/network/user-cards/author'
-          },
-          {
-            title: 'NFT',
-            path: '/network/user-cards/nft'
-          },
-          {
-            title: 'Social',
-            path: '/network/user-cards/social'
-          }
-        ]
+        title: 'Linked Geofence',
+       
       },
       {
-        title: 'User Table',
-        children: [
-          {
-            title: 'Team Crew',
-            path: '/network/user-table/team-crew'
-          },
-          {
-            title: 'App Roster',
-            path: '/network/user-table/app-roster'
-          },
-          {
-            title: 'Market Authors',
-            path: '/network/user-table/market-authors'
-          },
-          {
-            title: 'SaaS Users',
-            path: '/network/user-table/saas-users'
-          },
-          {
-            title: 'Store Clients',
-            path: '/network/user-table/store-clients'
-          },
-          {
-            title: 'Visitors',
-            path: '/network/user-table/visitors'
-          }
-        ]
+        title: 'Add Geofence',
+        
       },
-      {
-        title: 'Cooperations',
-        path: '/network/cooperations',
-        disabled: true
-      },
-      {
-        title: 'Leads',
-        path: '/network/leads',
-        disabled: true
-      },
-      {
-        title: 'Donators',
-        path: '/network/donators',
-        disabled: true
-      }
+      
     ]
   },
   {
-    title: 'Authentication',
+    title: 'Vehicle',
     icon: 'security-user',
     children: [
       {
         title: 'Classic',
         children: [
           {
-            title: 'Sign In',
-            path: '/auth/classic/login'
+            title: 'Vehicle',
+            path: '/vehicle/classic/login'
           },
           {
-            title: 'Sign Up',
-            path: '/auth/classic/signup'
-          },
-          {
-            title: '2FA',
-            path: '/auth/classic/2fa'
-          },
-          {
-            title: 'Check Email',
-            path: '/auth/classic/check-email'
-          },
-          {
-            title: 'Reset Password',
-            children: [
-              {
-                title: 'Enter Email',
-                path: '/auth/classic/reset-password/enter-email'
-              },
-              {
-                title: 'Check Email',
-                path: '/auth/classic/reset-password/check-email'
-              },
-              {
-                title: 'Change Password',
-                path: '/auth/classic/reset-password/change'
-              },
-              {
-                title: 'Password Changed',
-                path: '/auth/classic/reset-password/changed'
-              }
-            ]
+            title: 'Add Vehicle',
+            path: '/vehicle/classic/signup'
           }
+          
         ]
       },
       {
@@ -435,38 +75,38 @@ export const MENU_SIDEBAR: TMenuConfig = [
         children: [
           {
             title: 'Sign In',
-            path: '/auth/login'
+            path: '/vehicle/login'
           },
           {
             title: 'Sign Up',
-            path: '/auth/signup'
+            path: '/vehicle/signup'
           },
           {
             title: '2FA',
-            path: '/auth/2fa'
+            path: '/vehicle/2fa'
           },
           {
             title: 'Check Email',
-            path: '/auth/check-email'
+            path: '/vehicle/check-email'
           },
           {
             title: 'Reset Password',
             children: [
               {
                 title: 'Enter Email',
-                path: '/auth/reset-password/enter-email'
+                path: '/vehicle/reset-password/enter-email'
               },
               {
                 title: 'Check Email',
-                path: '/auth/reset-password/check-email'
+                path: '/vehicle/reset-password/check-email'
               },
               {
                 title: 'Change Password',
-                path: '/auth/reset-password/change'
+                path: '/vehicle/reset-password/change'
               },
               {
                 title: 'Password Changed',
-                path: '/auth/reset-password/changed'
+                path: '/vehicle/reset-password/changed'
               }
             ]
           }
@@ -474,11 +114,11 @@ export const MENU_SIDEBAR: TMenuConfig = [
       },
       {
         title: 'Welcome Message',
-        path: '/auth/welcome-message'
+        path: '/vehicle/welcome-message'
       },
       {
         title: 'Account Deactivated',
-        path: '/auth/account-deactivated'
+        path: '/vehicle/account-deactivated'
       },
       {
         title: 'Error 404',
@@ -490,32 +130,104 @@ export const MENU_SIDEBAR: TMenuConfig = [
       }
     ]
   },
-  { heading: 'Apps' },
+
   {
-    title: 'User Management',
-    icon: 'users',
-    disabled: true
+    title: 'Reservations',
+    icon: 'users', 
+    children: [
+      {
+        title: 'Add reservations',
+        path: '/vehicle/login'
+      },
+      {
+        title: 'Reservations',
+        path: '/vehicle/signup'
+      },
+
+
+    ]
+    
   },
   {
-    title: 'Projects',
+    title: 'Customer',
     icon: 'questionnaire-tablet',
-    disabled: true
+    children: [
+      {
+        title: 'Customer',
+        path: '/customer/customer'
+      },
+      {
+        title: 'Add Customer',
+        path: '/customer/add-customer'
+      },
+
+     
+    ]
+
   },
   {
-    title: 'eCommerce',
+    title: 'Maintenance',
     icon: 'handcart',
-    disabled: true
+    children: [
+      {
+        title: 'Maintenance',
+        path: '/maintenance/maintenance'
+      },
+      {
+        title: 'Add Maintenance',
+        path: '/maintenance/add-maintenance'
+      },
+
+     
+    ]
   },
-  { heading: 'Miscellaneous' },
   {
-    title: 'Modals',
+    title: 'Device',
     icon: 'some-files',
-    disabled: true
+    children: [
+      {
+        title: 'Device',
+        path: '/device/device'
+      },
+      {
+        title: 'Add device',
+        path: '/device/add-device'
+      },
+
+     
+    ]
   },
   {
-    title: 'Wizards',
+    title: 'User',
     icon: 'note-2',
-    disabled: true
+    children: [
+      {
+        title: 'User',
+        path: '/users/user'
+      },
+      {
+        title: 'Add users',
+        path: '/users/add-users'
+      },
+
+     
+    ]
+  },
+  {
+    title: 'Driver',
+    icon: 'note-2',
+    children: [
+      {
+        title: 'driver',
+        path: '/driver/driver'
+      },
+      {
+        title: 'Add Driver',
+        path: '/driver/add-driver'
+      },
+
+     
+    ]
   }
 ];
 
@@ -828,28 +540,28 @@ export const MENU_MEGA: TMenuConfig = [
           {
             title: 'Classic Layout',
             children: [
-              { title: 'Sign In', path: '/auth/classic/login' },
-              { title: 'Sign Up', path: '/auth/classic/signup' },
-              { title: '2FA', path: '/auth/classic/2fa' },
-              { title: 'Check Email', path: '/auth/classic/check-email' },
+              { title: 'Sign In', path: '/vehicle/classic/login' },
+              { title: 'Sign Up', path: '/vehicle/classic/signup' },
+              { title: '2FA', path: '/vehicle/classic/2fa' },
+              { title: 'Check Email', path: '/vehicle/classic/check-email' },
               {
                 title: 'Reset Password',
                 children: [
                   {
                     title: 'Enter Email',
-                    path: '/auth/classic/reset-password/enter-email'
+                    path: '/vehicle/classic/reset-password/enter-email'
                   },
                   {
                     title: 'Check Email',
-                    path: '/auth/classic/reset-password/check-email'
+                    path: '/vehicle/classic/reset-password/check-email'
                   },
                   {
                     title: 'Change Password',
-                    path: '/auth/classic/reset-password/change'
+                    path: '/vehicle/classic/reset-password/change'
                   },
                   {
                     title: 'Password is Changed',
-                    path: '/auth/classic/reset-password/changed'
+                    path: '/vehicle/classic/reset-password/changed'
                   }
                 ]
               }
@@ -858,28 +570,28 @@ export const MENU_MEGA: TMenuConfig = [
           {
             title: 'Branded Layout',
             children: [
-              { title: 'Sign In', path: '/auth/login' },
-              { title: 'Sign Up', path: '/auth/signup' },
-              { title: '2FA', path: '/auth/2fa' },
-              { title: 'Check Email', path: '/auth/check-email' },
+              { title: 'Sign In', path: '/vehicle/login' },
+              { title: 'Sign Up', path: '/vehicle/signup' },
+              { title: '2FA', path: '/vehicle/2fa' },
+              { title: 'Check Email', path: '/vehicle/check-email' },
               {
                 title: 'Reset Password',
                 children: [
                   {
                     title: 'Enter Email',
-                    path: '/auth/reset-password/enter-email'
+                    path: '/vehicle/reset-password/enter-email'
                   },
                   {
                     title: 'Check Email',
-                    path: '/auth/reset-password/check-email'
+                    path: '/vehicle/reset-password/check-email'
                   },
                   {
                     title: 'Change Password',
-                    path: '/auth/reset-password/change'
+                    path: '/vehicle/reset-password/change'
                   },
                   {
                     title: 'Password is Changed',
-                    path: '/auth/reset-password/changed'
+                    path: '/vehicle/reset-password/changed'
                   }
                 ]
               }
@@ -890,11 +602,11 @@ export const MENU_MEGA: TMenuConfig = [
       {
         title: 'Other Pages',
         children: [
-          { title: 'Welcome Message', icon: 'like-2', path: '/auth/welcome-message' },
+          { title: 'Welcome Message', icon: 'like-2', path: '/vehicle/welcome-message' },
           {
             title: 'Account Deactivated',
             icon: 'shield-cross',
-            path: '/auth/account-deactivated'
+            path: '/vehicle/account-deactivated'
           },
           { title: 'Error 404', icon: 'message-question', path: '/error/404' },
           { title: 'Error 500', icon: 'information', path: '/error/500' }
