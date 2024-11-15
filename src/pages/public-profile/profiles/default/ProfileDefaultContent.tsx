70c30d23876e941e22210cc2aa6bffada9500297
baraah-berra <baraahalsaidramadan@gmail.com>
2024-11-15 12:09:19 +0300

    delete not used imports

--- src/pages/public-profile/profiles/default/ProfileDefaultContent.tsx
+++ src/pages/public-profile/profiles/default/ProfileDefaultContent.tsx
@@ -1,21 +1,9 @@
-import {
-  About,
-  CommunityBadges,
-  Connections,
-  Contributions,
-  MediaUploads,
-  Projects,
-  RecentUploads,
-  Tags,
-  UnlockPartnerships,
-  WorkExperience
-} from './blocks';
+import { Projects } from './blocks';
 
 const ProfileDefaultContent = () => {
   return (
-      <div className="grid gap-5 lg:gap-7.5">
-        <Projects />
-     
+    <div className="grid gap-5 lg:gap-7.5">
+      <Projects />
     </div>
   );
 };
