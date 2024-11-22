44a03cdfce42f9074585ea08888a951ea2584cd8
baraah-berra <baraahalsaidramadan@gmail.com>
2024-11-22 13:16:12 +0300

    create user details component

---
+++ src/pages/public-profile/profiles/user/blocks/UserDetailsPage.tsx
@@ -0,0 +1,275 @@
+import React from 'react';
+
+const UserDetailsPage = () => {
+  return (
+    <div className="card max-w-4xl mx-auto bg-white shadow-md rounded-lg overflow-hidden">
+      <div className="card-body p-8">
+        <div className="flex">
+          <div className="w-1/3 border-r border-gray-200 pr-8">
+            <img
+              className="w-full rounded-lg object-cover mb-4"
+              src="https://via.placeholder.com/150"
+              alt="User"
+            />
+            <h2 className="text-2xl font-bold text-gray-800">Brad Dennis</h2>
+            <p className="text-gray-400">Turkish</p>
+          </div>
+
+          <div className="w-1/3 px-8">
+            <div className="mb-6">
+              <p className="text-gray-400 mb-1">Identify Number</p>
+              <p className="text-gray-800 font-semibold">5156156</p>
+            </div>
+            <div className="mb-6">
+              <p className="text-gray-400 mb-1">Subscription Start Date</p>
+              <p className="text-gray-800 font-semibold">01/06/1993</p>
+            </div>
+            <div className="mb-6">
+              <p className="text-gray-400 mb-1">Phone</p>
+              <p className="text-gray-800 font-semibold flex items-center">
+                <svg
+                  width="17"
+                  height="17"
+                  viewBox="0 0 17 17"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clip-path="url(#clip0_711_15508)">
+                    <path
+                      d="M14.4413 11.1691L12.741 9.46885C12.1338 8.86161 11.1015 9.10453 10.8586 9.89391C10.6764 10.4405 10.0692 10.7441 9.52268 10.6226C8.30821 10.319 6.66866 8.74016 6.36505 7.46496C6.18287 6.91843 6.54722 6.31119 7.09373 6.12904C7.88314 5.88615 8.12603 4.85384 7.5188 4.2466L5.81853 2.54634C5.33274 2.12127 4.60406 2.12127 4.17899 2.54634L3.02524 3.70009C1.87149 4.91457 3.14669 8.13292 6.0007 10.9869C8.85472 13.841 12.0731 15.1769 13.2876 13.9624L14.4413 12.8087C14.8664 12.3229 14.8664 11.5942 14.4413 11.1691Z"
+                      fill="#5E6278"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_711_15508">
+                      <rect width="17" height="17" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+                +90954948849
+              </p>
+            </div>
+            <div className="mb-6">
+              <p className="text-gray-400 mb-1">Country</p>
+              <p className="text-gray-800 font-semibold flex items-center">
+                <svg
+                  width="17"
+                  height="17"
+                  viewBox="0 0 17 17"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clip-path="url(#clip0_711_15602)">
+                    <g clip-path="url(#clip1_711_15602)">
+                      <g clip-path="url(#clip2_711_15602)">
+                        <path
+                          d="M14.5853 14.4434V10.1072H9.915V14.4434H9.1021V9.2943H11.8437V4.65821H10.9299V2.84189H9.89576V1.38281H9.08286V2.84189H8.04874V4.65821H7.13488V14.4434H6.32197V6.47456H2.7765V14.4434H1.74414V15.2563H15.6177V14.4434H14.5853ZM9.99101 5.66166H10.8039V6.47456H9.99101V5.66166ZM9.99101 7.47798H10.8039V8.29088H9.99101V7.47798ZM4.93774 14.1247H4.12483V13.3117H4.93774V14.1247ZM4.93774 12.1801H4.12483V11.3672H4.93774V12.1801ZM4.93774 10.2355H4.12483V9.42258H4.93774V10.2355ZM4.93774 8.29088H4.12483V7.47798H4.93774V8.29088ZM8.98759 8.29088H8.17469V7.47798H8.98759V8.29088ZM8.98759 6.47456H8.17469V5.66166H8.98759V6.47456ZM9.08283 3.84531H9.89574V4.65821H9.08283V3.84531ZM11.7428 14.4434H10.9299V11.181H11.7428V14.4434ZM13.5705 14.4434H12.7576V11.181H13.5705V14.4434Z"
+                          fill="#5E6278"
+                        />
+                      </g>
+                    </g>
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_711_15602">
+                      <rect width="17" height="17" fill="white" />
+                    </clipPath>
+                    <clipPath id="clip1_711_15602">
+                      <rect
+                        width="13.7447"
+                        height="13.7447"
+                        fill="white"
+                        transform="translate(1.80859 1.44727)"
+                      />
+                    </clipPath>
+                    <clipPath id="clip2_711_15602">
+                      <rect
+                        width="13.8735"
+                        height="13.8735"
+                        fill="white"
+                        transform="translate(1.74414 1.38281)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+                Turkey
+              </p>
+            </div>
+            <div className="mb-6">
+              <p className="text-gray-400 mb-1">Area</p>
+              <p className="text-gray-800 font-semibold flex items-center">
+                <svg
+                  width="17"
+                  height="17"
+                  viewBox="0 0 17 17"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clip-path="url(#clip0_711_15602)">
+                    <g clip-path="url(#clip1_711_15602)">
+                      <g clip-path="url(#clip2_711_15602)">
+                        <path
+                          d="M14.5853 14.4434V10.1072H9.915V14.4434H9.1021V9.2943H11.8437V4.65821H10.9299V2.84189H9.89576V1.38281H9.08286V2.84189H8.04874V4.65821H7.13488V14.4434H6.32197V6.47456H2.7765V14.4434H1.74414V15.2563H15.6177V14.4434H14.5853ZM9.99101 5.66166H10.8039V6.47456H9.99101V5.66166ZM9.99101 7.47798H10.8039V8.29088H9.99101V7.47798ZM4.93774 14.1247H4.12483V13.3117H4.93774V14.1247ZM4.93774 12.1801H4.12483V11.3672H4.93774V12.1801ZM4.93774 10.2355H4.12483V9.42258H4.93774V10.2355ZM4.93774 8.29088H4.12483V7.47798H4.93774V8.29088ZM8.98759 8.29088H8.17469V7.47798H8.98759V8.29088ZM8.98759 6.47456H8.17469V5.66166H8.98759V6.47456ZM9.08283 3.84531H9.89574V4.65821H9.08283V3.84531ZM11.7428 14.4434H10.9299V11.181H11.7428V14.4434ZM13.5705 14.4434H12.7576V11.181H13.5705V14.4434Z"
+                          fill="#5E6278"
+                        />
+                      </g>
+                    </g>
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_711_15602">
+                      <rect width="17" height="17" fill="white" />
+                    </clipPath>
+                    <clipPath id="clip1_711_15602">
+                      <rect
+                        width="13.7447"
+                        height="13.7447"
+                        fill="white"
+                        transform="translate(1.80859 1.44727)"
+                      />
+                    </clipPath>
+                    <clipPath id="clip2_711_15602">
+                      <rect
+                        width="13.8735"
+                        height="13.8735"
+                        fill="white"
+                        transform="translate(1.74414 1.38281)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+                Turkey
+              </p>
+            </div>
+          </div>
+
+          <div className="w-1/3 px-8">
+            <div className="mb-6">
+              <p className="text-gray-400 mb-1">Time Zone</p>
+              <p className="text-gray-800 font-semibold">Time Zone</p>
+            </div>
+            <div className="mb-6">
+              <p className="text-gray-400 mb-1">Role</p>
+              <p className="text-gray-800 font-semibold">Role</p>
+            </div>
+            <div className="mb-6">
+              <p className="text-gray-400 mb-1">Phone2</p>
+              <p className="text-gray-800 font-semibold flex items-center">
+                <svg
+                  width="17"
+                  height="17"
+                  viewBox="0 0 17 17"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clip-path="url(#clip0_711_15508)">
+                    <path
+                      d="M14.4413 11.1691L12.741 9.46885C12.1338 8.86161 11.1015 9.10453 10.8586 9.89391C10.6764 10.4405 10.0692 10.7441 9.52268 10.6226C8.30821 10.319 6.66866 8.74016 6.36505 7.46496C6.18287 6.91843 6.54722 6.31119 7.09373 6.12904C7.88314 5.88615 8.12603 4.85384 7.5188 4.2466L5.81853 2.54634C5.33274 2.12127 4.60406 2.12127 4.17899 2.54634L3.02524 3.70009C1.87149 4.91457 3.14669 8.13292 6.0007 10.9869C8.85472 13.841 12.0731 15.1769 13.2876 13.9624L14.4413 12.8087C14.8664 12.3229 14.8664 11.5942 14.4413 11.1691Z"
+                      fill="#5E6278"
+                    />
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_711_15508">
+                      <rect width="17" height="17" fill="white" />
+                    </clipPath>
+                  </defs>
+                </svg>
+                +90954948849
+              </p>
+            </div>
+            <div className="mb-6">
+              <p className="text-gray-400 mb-1">City</p>
+              <p className="text-gray-800 font-semibold flex items-center">
+                <svg
+                  width="17"
+                  height="17"
+                  viewBox="0 0 17 17"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clip-path="url(#clip0_711_15602)">
+                    <g clip-path="url(#clip1_711_15602)">
+                      <g clip-path="url(#clip2_711_15602)">
+                        <path
+                          d="M14.5853 14.4434V10.1072H9.915V14.4434H9.1021V9.2943H11.8437V4.65821H10.9299V2.84189H9.89576V1.38281H9.08286V2.84189H8.04874V4.65821H7.13488V14.4434H6.32197V6.47456H2.7765V14.4434H1.74414V15.2563H15.6177V14.4434H14.5853ZM9.99101 5.66166H10.8039V6.47456H9.99101V5.66166ZM9.99101 7.47798H10.8039V8.29088H9.99101V7.47798ZM4.93774 14.1247H4.12483V13.3117H4.93774V14.1247ZM4.93774 12.1801H4.12483V11.3672H4.93774V12.1801ZM4.93774 10.2355H4.12483V9.42258H4.93774V10.2355ZM4.93774 8.29088H4.12483V7.47798H4.93774V8.29088ZM8.98759 8.29088H8.17469V7.47798H8.98759V8.29088ZM8.98759 6.47456H8.17469V5.66166H8.98759V6.47456ZM9.08283 3.84531H9.89574V4.65821H9.08283V3.84531ZM11.7428 14.4434H10.9299V11.181H11.7428V14.4434ZM13.5705 14.4434H12.7576V11.181H13.5705V14.4434Z"
+                          fill="#5E6278"
+                        />
+                      </g>
+                    </g>
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_711_15602">
+                      <rect width="17" height="17" fill="white" />
+                    </clipPath>
+                    <clipPath id="clip1_711_15602">
+                      <rect
+                        width="13.7447"
+                        height="13.7447"
+                        fill="white"
+                        transform="translate(1.80859 1.44727)"
+                      />
+                    </clipPath>
+                    <clipPath id="clip2_711_15602">
+                      <rect
+                        width="13.8735"
+                        height="13.8735"
+                        fill="white"
+                        transform="translate(1.74414 1.38281)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+                Istanbul
+              </p>
+            </div>
+            <div className="mb-6">
+              <p className="text-gray-400 mb-1">Address In Detail</p>
+              <p className="text-gray-800 font-semibold flex items-center">
+                <svg
+                  width="17"
+                  height="17"
+                  viewBox="0 0 17 17"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <g clip-path="url(#clip0_711_15602)">
+                    <g clip-path="url(#clip1_711_15602)">
+                      <g clip-path="url(#clip2_711_15602)">
+                        <path
+                          d="M14.5853 14.4434V10.1072H9.915V14.4434H9.1021V9.2943H11.8437V4.65821H10.9299V2.84189H9.89576V1.38281H9.08286V2.84189H8.04874V4.65821H7.13488V14.4434H6.32197V6.47456H2.7765V14.4434H1.74414V15.2563H15.6177V14.4434H14.5853ZM9.99101 5.66166H10.8039V6.47456H9.99101V5.66166ZM9.99101 7.47798H10.8039V8.29088H9.99101V7.47798ZM4.93774 14.1247H4.12483V13.3117H4.93774V14.1247ZM4.93774 12.1801H4.12483V11.3672H4.93774V12.1801ZM4.93774 10.2355H4.12483V9.42258H4.93774V10.2355ZM4.93774 8.29088H4.12483V7.47798H4.93774V8.29088ZM8.98759 8.29088H8.17469V7.47798H8.98759V8.29088ZM8.98759 6.47456H8.17469V5.66166H8.98759V6.47456ZM9.08283 3.84531H9.89574V4.65821H9.08283V3.84531ZM11.7428 14.4434H10.9299V11.181H11.7428V14.4434ZM13.5705 14.4434H12.7576V11.181H13.5705V14.4434Z"
+                          fill="#5E6278"
+                        />
+                      </g>
+                    </g>
+                  </g>
+                  <defs>
+                    <clipPath id="clip0_711_15602">
+                      <rect width="17" height="17" fill="white" />
+                    </clipPath>
+                    <clipPath id="clip1_711_15602">
+                      <rect
+                        width="13.7447"
+                        height="13.7447"
+                        fill="white"
+                        transform="translate(1.80859 1.44727)"
+                      />
+                    </clipPath>
+                    <clipPath id="clip2_711_15602">
+                      <rect
+                        width="13.8735"
+                        height="13.8735"
+                        fill="white"
+                        transform="translate(1.74414 1.38281)"
+                      />
+                    </clipPath>
+                  </defs>
+                </svg>
+                Istanbul
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserDetailsPage;
