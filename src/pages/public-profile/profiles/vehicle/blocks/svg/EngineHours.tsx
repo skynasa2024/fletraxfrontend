de39e8d0fc02ae42513a6e6e2dfc7d8819ecf659
baraah-berra <baraahalsaidramadan@gmail.com>
2024-11-28 14:09:53 +0300

    add icons

---
+++ src/pages/public-profile/profiles/vehicle/blocks/svg/EngineHours.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const EngineHours: React.FC = () => {
+  return (
+    <svg width="39" height="38" viewBox="0 0 39 38" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="19.5" cy="19" r="19" fill="#FF7E86" fill-opacity="0.1"/>
+<path d="M19.9164 26.9215L23.0197 23.8182L21.7237 22.5054L20.1892 24.0399L20.1891 14.1503L21.7236 15.6848L23.0366 14.3719L19.9162 11.2686C19.5581 10.9105 18.9614 10.9105 18.6033 11.2686L15.5 14.3719L16.813 15.6848L18.3475 14.1503V24.0399L16.813 22.5054L15.5 23.8183L18.6033 26.9216C18.9784 27.2795 19.5581 27.2795 19.9162 26.9214L19.9164 26.9215Z" fill="#FF7E86"/>
+</svg>
+
+    
+  );
+};
+
+export default EngineHours;
