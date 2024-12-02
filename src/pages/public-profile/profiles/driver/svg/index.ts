f2a5039a52321832784a43018eb39148dfc39b57
baraah-berra <baraahalsaidramadan@gmail.com>
2024-12-02 22:21:54 +0300

    Update index.ts

--- src/pages/public-profile/profiles/driver/svg/index.ts
+++ src/pages/public-profile/profiles/driver/svg/index.ts
@@ -9,17 +9,12 @@ export { default as ModelSeries } from './ModelSeries';
 export { default as ModelYear } from './ModelYear';
 export { default as Power } from './Power';
 export { default as Tayp } from './Tayp';
-
-
 export { default as Volume } from './Volume'
-
-
 export { default as Mileage } from './Mileage';
 export { default as RentedTimes } from './RentedTimes';
-
 export { default as MapPin } from './MapPin';
 export { default as PhoneIcon } from './PhoneIcon';
 export { default as MailIcon } from './MailIcon';
 export { default as CalendarIcon } from './CalendarIcon';
 export { default as CountryCityIcon } from './CountryCityIcon';
-export { default as MapIcon } from './MApIcon';
+export { default as MapIcon } from './MapIcon';
