60ff7ac12910b107c568575c241a8d2beb28d091
baraah-berra <baraahalsaidramadan@gmail.com>
2024-12-18 01:40:47 +0300

    update log in page

--- src/layouts/auth-branded/AuthBrandedLayout.tsx
+++ src/layouts/auth-branded/AuthBrandedLayout.tsx
@@ -13,7 +13,7 @@ const Layout = () => {
       <style>
         {`
           .branded-bg {
-            background-image: url('${toAbsoluteUrl('/media/images/2600x1600/1.png')}');
+            background-image: url('${toAbsoluteUrl('/media/images/2600x1600/1.svg')}');
           }
           .dark .branded-bg {
             background-image: url('${toAbsoluteUrl('/media/images/2600x1600/1-dark.png')}');
@@ -28,24 +28,8 @@ const Layout = () => {
 
         <div className="lg:rounded-xl lg:border lg:border-gray-200 lg:m-5 order-1 lg:order-2 bg-top xxl:bg-center xl:bg-cover bg-no-repeat branded-bg">
           <div className="flex flex-col p-8 lg:p-16 gap-4">
-            <Link to="/">
-              <img
-                src={toAbsoluteUrl('/media/app/mini-logo.svg')}
-                className="h-[28px] max-w-none"
-                alt=""
-              />
-            </Link>
-
-            <div className="flex flex-col gap-3">
-              <h3 className="text-2xl font-semibold text-gray-900">Secure Access Portal</h3>
-              <div className="text-base font-medium text-gray-600">
-                A robust authentication gateway ensuring
-                <br /> secure&nbsp;
-                <span className="text-gray-900 font-semibold">efficient user access</span>
-                &nbsp;to the Metronic
-                <br /> Dashboard interface.
-              </div>
-            </div>
+            
+           
           </div>
         </div>
       </div>
