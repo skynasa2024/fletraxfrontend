b1e914f08b9061954ee5599bdff23159dd4dc96f
baraah-berra <baraahalsaidramadan@gmail.com>
2024-12-19 20:39:38 +0300

    shadows hover and cards for vehicle details

--- src/pages/public-profile/profiles/vehicle/blocks/VehicleDetailsPage.tsx
+++ src/pages/public-profile/profiles/vehicle/blocks/VehicleDetailsPage.tsx
@@ -95,9 +95,9 @@ const VehicleInfoCards = () => {
   return (
     <>
       <Toolbar />
-      <div className="w-full mx-auto px-4">
+      <div className="w-full mx-auto px-4 ">
         <div className="flex flex-col lg:flex-row">
-          <div className="card-body w-full lg:w-1/2 bg-white shadow-md rounded-lg mx-2 my-4 px-5 grid grid-cols-1">
+          <div className="card hover:shadow-md w-full lg:w-1/2 rounded-lg grid grid-cols-1 mb-2">
             <div className="grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-3">
               {/* Car Plate */}
               <div className="flex items-center justify-center mb-4">
@@ -118,7 +118,7 @@ const VehicleInfoCards = () => {
               </div>
 
               {/* Device Section */}
-              <div className="flex items-center space-x-4 mb-4">
+              <div className="flex items-center space-x-4  mb-4">
                 <div className="bg-blue-100 p-2 rounded-full"></div>
                 <div>
                   <span className="text-lg font-medium">41643739</span>
@@ -135,25 +135,25 @@ const VehicleInfoCards = () => {
                 />
                 {/* Plus buttons */}
                 <button
-                  className="absolute top-2 left-2 border bg-white text-gray rounded-full w-8 h-8 flex items-center justify-center"
+                  className="absolute top-2 left-2 border hover:shadow-md text-gray rounded-full w-8 h-8 flex items-center justify-center"
                   onClick={() => alert('Add input here')}
                 >
                   +
                 </button>
                 <button
-                  className="absolute top-2 right-2 border bg-white text-gray rounded-full w-8 h-8 flex items-center justify-center"
+                  className="absolute top-2 right-2 border hover:shadow-md text-gray rounded-full w-8 h-8 flex items-center justify-center"
                   onClick={() => alert('Add input here')}
                 >
                   +
                 </button>
                 <button
-                  className="absolute bottom-2 left-2 border bg-white text-gray rounded-full w-8 h-8 flex items-center justify-center"
+                  className="absolute bottom-2 left-2 border hover:shadow-md text-gray rounded-full w-8 h-8 flex items-center justify-center"
                   onClick={() => alert('Add input here')}
                 >
                   +
                 </button>
                 <button
-                  className="absolute bottom-2 right-2 border bg-white text-gray rounded-full w-8 h-8 flex items-center justify-center"
+                  className="absolute bottom-2 right-2 border hover:shadow-md text-gray rounded-full w-8 h-8 flex items-center justify-center"
                   onClick={() => alert('Add input here')}
                 >
                   +
@@ -161,17 +161,16 @@ const VehicleInfoCards = () => {
               </div>
             </div>
           </div>
-
-          <div className="container mx-auto p-5 grid gap-5 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
+          <div className="container mx-auto p-5  grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1">
             {/* Vehicle Info Section */}
-            <div className="flex flex-col lg:flex-row h-full">
+            <div className="flex flex-col lg:flex-row h-full mb-4 ">
               {/* Vehicle Info Cards */}
-              <div className="flex-grow bg-white mr-0 lg:mr-4 mb-4 lg:mb-0">
+              <div className="flex-grow mr-0 lg:mr-4">
                 <div className="grid grid-cols-3 sm:grid-cols-3 lg:grid-cols-3 gap-4">
                   {vehicleInfo.map(({ label, value, Icon }, index) => (
                     <div
                       key={index}
-                      className="p-4 rounded-lg shadow-sm border hover:shadow-md flex flex-col h-40"
+                      className="card p-4 rounded-lg hover:shadow-md flex flex-col h-40"
                     >
                       <Icon className="w-6 h-6 text-gray-400" />
                       <span className="text-sm text-gray-500 font-medium my-2">{label}</span>
@@ -181,57 +180,58 @@ const VehicleInfoCards = () => {
                 </div>
               </div>
 
-              {/* Map and Details */}
-              <div className="flex-grow lg:w-1/3 bg-white shadow-md rounded-lg overflow-hidden">
-                <div className="p-4 h-full flex flex-col">
-                  <img
-                    className="w-full rounded-lg object-cover h-40 mb-4"
-                    src="https://via.placeholder.com/150"
-                    alt="Map"
-                  />
-                  <div className="flex-grow">
-                    {details.map(({ label, value }, index) => (
-                      <div key={index} className="flex items-start mb-2">
-                        <span className="text-gray-400 mr-1">{label}:</span>
-                        <span className="text-gray-800">{value}</span>
-                      </div>
-                    ))}
-                  </div>
+              {/* Details */}
+              <div className="flex-grow lg:w-1/3 mb-4 card hover:shadow-md overflow-hidden p-4 flex flex-col">
+                <img
+                  className="w-full rounded-lg object-cover h-40 mb-4"
+                  src="https://via.placeholder.com/150"
+                  alt="Map"
+                />
+                <div className="flex-grow">
+                  {details.map(({ label, value }, index) => (
+                    <div key={index} className="flex items-start mb-2">
+                      <span className="text-gray-400 mr-1">{label}:</span>
+                      <span className="text-gray-800">{value}</span>
+                    </div>
+                  ))}
                 </div>
               </div>
             </div>
 
             {/* Inspection and Insurance Section */}
-            <div className="bg-white rounded-lg shadow p-6">
-              <h2 className="text-xl font-semibold mb-4">Vehicle Inspection and Insurance</h2>
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div className="card hover:shadow-md mb-2 p-4">
+              <h2 className="text-xl font-semibold mb-2">Vehicle Inspection and Insurance</h2>
+              <div className="grid grid-cols-1 sm:grid-cols-2">
                 {items.map((item) => (
-                  <div
-                    key={item.title}
-                    className="flex items-start space-x-3 p-4 bg-gray-50 rounded-lg"
-                  >
-                    <svg
-                      width={32}
-                      height={32}
-                      viewBox="0 0 32 32"
-                      fill="none"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        d="M3.55566 7.40137V13.9414C3.55469 20.876 7.90332 27.209 14.3779 29.6992C14.9004 29.9004 15.4502 30 16 30C16.5498 30 17.0996 29.8994 17.623 29.6992L18.5938 29.3252C24.5781 27.0234 28.4453 21.2344 28.4443 14.5781V7.40137C28.4443 6.86426 28.0195 6.42285 27.4834 6.40234C23.8887 6.2627 20.2236 4.8457 16.5898 2.19238C16.2383 1.93652 15.7617 1.93652 15.4102 2.19238C11.7754 4.8457 8.11035 6.2627 4.5166 6.40234C3.98047 6.42285 3.55566 6.86426 3.55566 7.40137ZM5.55566 8.33691C9.04004 8.00781 12.5459 6.62891 16 4.22754C19.4531 6.62891 22.96 8.00781 26.4443 8.33691V14.5781C26.4453 20.4004 23.082 25.4561 17.875 27.458L16.9053 27.832C16.3213 28.0566 15.6777 28.0566 15.0957 27.832C9.38867 25.6367 5.55469 20.0547 5.55566 13.9414V8.33691Z"
-                        fill="#FF3C3C"
-                      />
-                      <path
-                        d="M13.9521 20.1309C14.1396 20.3184 14.3936 20.4238 14.6592 20.4238C14.9248 20.4238 15.1787 20.3184 15.3662 20.1309L21.5859 13.9111C21.9766 13.5205 21.9766 12.8877 21.5859 12.4971C21.1953 12.1064 20.5625 12.1064 20.1719 12.4971L14.6592 18.0098L11.8281 15.1777C11.4375 14.7871 10.8047 14.7871 10.4141 15.1777C10.0234 15.5684 10.0234 16.2012 10.4141 16.5918L13.9521 20.1309Z"
-                        fill="#DA0806"
-                      />
-                    </svg>
-
+                  <div key={item.title} className="flex items-start space-x-3 mb-1 rounded-lg">
+                    {' '}
+                    <div className="rounded-lg p-3 bg-gray-200">
+                      <svg
+                        width={32}
+                        height={32}
+                        viewBox="0 0 32 32"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M3.55566 7.40137V13.9414C3.55469 20.876 7.90332 27.209 14.3779 29.6992C14.9004 29.9004 15.4502 30 16 30C16.5498 30 17.0996 29.8994 17.623 29.6992L18.5938 29.3252C24.5781 27.0234 28.4453 21.2344 28.4443 14.5781V7.40137C28.4443 6.86426 28.0195 6.42285 27.4834 6.40234C23.8887 6.2627 20.2236 4.8457 16.5898 2.19238C16.2383 1.93652 15.7617 1.93652 15.4102 2.19238C11.7754 4.8457 8.11035 6.2627 4.5166 6.40234C3.98047 6.42285 3.55566 6.86426 3.55566 7.40137ZM5.55566 8.33691C9.04004 8.00781 12.5459 6.62891 16 4.22754C19.4531 6.62891 22.96 8.00781 26.4443 8.33691V14.5781C26.4453 20.4004 23.082 25.4561 17.875 27.458L16.9053 27.832C16.3213 28.0566 15.6777 28.0566 15.0957 27.832C9.38867 25.6367 5.55469 20.0547 5.55566 13.9414V8.33691Z"
+                          fill="#FF3C3C"
+                        />
+                        <path
+                          d="M13.9521 20.1309C14.1396 20.3184 14.3936 20.4238 14.6592 20.4238C14.9248 20.4238 15.1787 20.3184 15.3662 20.1309L21.5859 13.9111C21.9766 13.5205 21.9766 12.8877 21.5859 12.4971C21.1953 12.1064 20.5625 12.1064 20.1719 12.4971L14.6592 18.0098L11.8281 15.1777C11.4375 14.7871 10.8047 14.7871 10.4141 15.1777C10.0234 15.5684 10.0234 16.2012 10.4141 16.5918L13.9521 20.1309Z"
+                          fill="#DA0806"
+                        />
+                      </svg>
+                    </div>
                     <div>
-                      <h3 className="font-medium">{item.title}</h3>
-                      <div className="mt-2 text-sm text-gray-600 space-y-1">
-                        <div>Start: {item.startDate}</div>
-                        <div>End: {item.endDate}</div>
+                      <h3 className="font-semibold text-lg ">{item.title}</h3>
+                      <div className="mt-1 space-y-0.5">
+                        <div className="text-sm text-gray-500">
+                          Start : <span className="ml-1">{item.startDate}</span>
+                        </div>
+                        <div className="text-sm text-gray-500">
+                          End : <span className="ml-1">{item.endDate}</span>
+                        </div>
                       </div>
                     </div>
                   </div>
@@ -240,11 +240,11 @@ const VehicleInfoCards = () => {
             </div>
           </div>
         </div>
-        <div className="flex flex-col mb-4 md:flex-row space-y-4 md:space-x-4 h-full w-600 m-2 mb-6 ">
+        <div className="flex flex-col mb-4 md:flex-row space-y-4 md:space-x-4 h-full w-600 m-2">
           <TripList trips={trips} totalTrips={450} className="sm:w-full mt-4" title="Trips" />
-          <div className="p-4 card w-full">map</div>
+          <div className="p-4 card hover:shadow-md  w-full">map</div>
         </div>
-        <div className="flex flex-grow mb-4 flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4 mb-8">
+        <div className="flex flex-grow mb-4 flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
           <div className="w-1/3">
             <h2 className="text-xl font-semibold mb-4 ps-4">Performance Metrics</h2>
             <FileList files={files} onView={(file) => console.log('Viewing file:', file.name)} />
@@ -259,14 +259,18 @@ const VehicleInfoCards = () => {
           />
         </div>
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
-          <GeofenceList
-            items={geofences}
-            title="Geofences"
-            className="mx-2"
-            onItemClick={(item) => console.log(`Selected: ${item}`)}
-            searchPlaceholder="Search..."
-          />
-          <MaintenanceViolationTable />
+          <div className="w-1/3">
+            <GeofenceList
+              items={geofences}
+              title="Geofences"
+              className="mx-2"
+              onItemClick={(item) => console.log(`Selected: ${item}`)}
+              searchPlaceholder="Search..."
+            />
+          </div>
+          <div className="w-2/3">
+            <MaintenanceViolationTable />
+          </div>
         </div>
       </div>
     </>
