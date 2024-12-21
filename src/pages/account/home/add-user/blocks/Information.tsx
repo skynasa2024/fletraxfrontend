9933d688ff5da4e945caa887fcb868dd2440bc8b
baraah-berra <baraahalsaidramadan@gmail.com>
2024-12-21 16:42:45 +0300

    calendar icon

--- src/pages/account/home/add-user/blocks/Information.tsx
+++ src/pages/account/home/add-user/blocks/Information.tsx
@@ -22,7 +22,7 @@ const Information = ({ title }: IGeneralSettingsProps) => {
           </div>
         </div>
         <div className="grid lg:grid-cols-2 gap-5">
-        <div className="grid gap-2.5">
+          <div className="grid gap-2.5">
             <label className="form-label">Subscription Start Date</label>
             <div className="relative">
               <input type="text" className="input w-full" placeholder="DD/MM/YYYY" />
@@ -110,24 +110,24 @@ const Information = ({ title }: IGeneralSettingsProps) => {
         <div className="grid gap-2.5">
           <label className="form-label">Photo</label>
           <div className="border-dashed border-2 border-gray-300 p-5 text-center">
-          <div className="flex flex-col items-center justify-center">
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M19.35 10.04C18.67 6.59 15.64 4 12 4C9.11 4 6.6 5.64 5.35 8.04C2.34 8.36 0 10.91 0 14C0 17.31 2.69 20 6 20H19C21.76 20 24 17.76 24 15C24 12.36 21.95 10.22 19.35 10.04ZM14 13V17H10V13H7L12 8L17 13H14Z"
-                    fill="#0F0F0F"
-                  />
-                </svg>
+            <div className="flex flex-col items-center justify-center">
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M19.35 10.04C18.67 6.59 15.64 4 12 4C9.11 4 6.6 5.64 5.35 8.04C2.34 8.36 0 10.91 0 14C0 17.31 2.69 20 6 20H19C21.76 20 24 17.76 24 15C24 12.36 21.95 10.22 19.35 10.04ZM14 13V17H10V13H7L12 8L17 13H14Z"
+                  fill="#0F0F0F"
+                />
+              </svg>
 
-                <p className="mb-1 text-sm text-gray-500">Drag and drop your files</p>
-                <p className="text-xs text-gray-400 mb-3">Unlimited files, 5GB total limit</p>
-                <button className="btn btn-primary btn-sm">Or choose files</button>
-              </div>
+              <p className="mb-1 text-sm text-gray-500">Drag and drop your files</p>
+              <p className="text-xs text-gray-400 mb-3">Unlimited files, 5GB total limit</p>
+              <button className="btn btn-primary btn-sm">Or choose files</button>
+            </div>
           </div>
         </div>
         <div className="grid gap-2.5">
