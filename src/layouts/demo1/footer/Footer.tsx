bda5fa79a47194cbc788c131532f7136c5437394
baraah-berra <baraahalsaidramadan@gmail.com>
2024-12-23 14:30:21 +0300

    comment footer

--- src/layouts/demo1/footer/Footer.tsx
+++ src/layouts/demo1/footer/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="footer">
       <Container>
-        <div className="flex flex-col md:flex-row justify-center md:justify-between items-center gap-3 py-5">
+       {/*  <div className="flex flex-col md:flex-row justify-center md:justify-between items-center gap-3 py-5">
           <div className="flex order-2 md:order-1  gap-2 font-normal text-2sm">
             <span className="text-gray-500">{currentYear} &copy;</span>
             <a
@@ -35,7 +35,7 @@ const Footer = () => {
               License
             </a>
           </nav>
-        </div>
+        </div> */}
       </Container>
     </footer>
   );
