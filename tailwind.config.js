57066aedf170a413a78e820465d90e1dcc6a9827
baraah-berra <baraahalsaidramadan@gmail.com>
2024-12-23 15:35:45 +0300

    dark mode cards border

--- tailwind.config.js
+++ tailwind.config.js
@@ -409,7 +409,7 @@ module.exports = {
               tooltip: '0'
             },
             dark: {
-              card: `1px solid ${theme('base.colors.gray.dark')['100']}`,
+              card: `1px solid ${theme('base.colors.gray.dark')['900']}`,
               table: `1px solid ${theme('base.colors.gray.dark')['100']}`,
               dropdown: `1px solid ${theme('base.colors.gray.dark')['100']}`,
               tooltip: `1px solid ${theme('base.colors.gray.dark')['100']}`,
