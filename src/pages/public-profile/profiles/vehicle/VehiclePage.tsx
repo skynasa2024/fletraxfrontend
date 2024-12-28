b44f860d0f94f46bdc6b545c3f21004702a9f8d9
Kareem Morsy <kreem.morsy@hotmail.com>
2024-12-28 11:46:35 +0200

    refactor: make user mini card more reusable to use in other pages

--- src/pages/public-profile/profiles/vehicle/VehiclePage.tsx
+++ src/pages/public-profile/profiles/vehicle/VehiclePage.tsx
@@ -1,12 +1,53 @@
+import { useNavigate } from 'react-router';
 import { VehicleList } from './blocks';
-import { UserMiniCards } from './mini-cards/UserMiniCards';
+import UserMiniCards, { MetricData } from './mini-cards/UserMiniCards';
+import BlocksIcon from './blocks/svg/BlocksIcon';
+import PeopleIcon from './blocks/svg/PeopleIcon';
+
+const metrics: MetricData[] = [
+  {
+    value: '250',
+    label: 'Total Vehicles',
+    textColor: 'text-white',
+    bgColor: 'bg-blue-500',
+    icon: <BlocksIcon />
+  },
+  {
+    value: '5',
+    label: 'Rented Vehicles',
+    textColor: 'text-gray-800',
+    icon: <PeopleIcon color="#FF0000" />
+  },
+  {
+    value: '25',
+    label: 'Vehicles in maintenance',
+    textColor: 'text-gray-800',
+    icon: <PeopleIcon color="#FFA800" />
+  },
+  {
+    value: '5',
+    label: 'Available For Rent',
+    textColor: 'text-gray-800',
+    icon: <PeopleIcon color="#5271FF" />
+  }
+];
 
 const VehiclePage = () => {
+  const navigate = useNavigate();
+
+
   return (
     <div className="grid gap-5 lg:gap-7.5">
       <h3 className="font-bold text-xl text-gray-800">Vehicle List</h3>
 
-      <UserMiniCards />
+      <button
+        className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg font-medium text-sm ml-auto"
+        onClick={() => navigate('/vehicles/add-vehicle')}
+      >
+        New Vehicle
+      </button>
+
+      <UserMiniCards metrics={metrics} />
       <VehicleList />
     </div>
   );
