9c8c4f7c38f3869596860be0b92fecf1748830e8
Kareem Morsy <kreem.morsy@hotmail.com>
2024-12-28 11:46:35 +0200

    refactor: make user mini card more reusable to use in other pages

--- src/pages/public-profile/profiles/vehicle/mini-cards/NetworkMiniCardsPage.tsx
+++ src/pages/public-profile/profiles/vehicle/mini-cards/NetworkMiniCardsPage.tsx
@@ -9,11 +9,43 @@ import {
   ToolbarPageTitle
 } from '@/partials/toolbar';
 
-import { UserMiniCards } from '.';
-import { useLayout } from '@/providers'; 
+import { useLayout } from '@/providers';
+import UserMiniCards, { MetricData } from './UserMiniCards';
+import BlocksIcon from '../blocks/svg/BlocksIcon';
+import PeopleIcon from '../blocks/svg/PeopleIcon';
+import { useNavigate } from 'react-router';
+
+const metrics: MetricData[] = [
+  {
+    value: '250',
+    label: 'Total Vehicles',
+    textColor: 'text-white',
+    bgColor: 'bg-blue-500',
+    icon: <BlocksIcon />
+  },
+  {
+    value: '5',
+    label: 'Rented Vehicles',
+    textColor: 'text-gray-800',
+    icon: <PeopleIcon color="#FF0000" />
+  },
+  {
+    value: '25',
+    label: 'Vehicles in maintenance',
+    textColor: 'text-gray-800',
+    icon: <PeopleIcon color="#FFA800" />
+  },
+  {
+    value: '5',
+    label: 'Available For Rent',
+    textColor: 'text-gray-800',
+    icon: <PeopleIcon color="#5271FF" />
+  }
+];
 
 const NetworkMiniCardsPage = () => {
-  const { currentLayout } = useLayout ();
+  const navigate = useNavigate();
+  const { currentLayout } = useLayout();
 
   return (
     <Fragment>
@@ -37,7 +69,13 @@ const NetworkMiniCardsPage = () => {
       )}
 
       <Container>
-        <UserMiniCards />
+        <button
+          className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg font-medium text-sm ml-auto"
+          onClick={() => navigate('/vehicles/add-vehicle')}
+        >
+          New Vehicle
+        </button>
+        <UserMiniCards metrics={metrics} />
       </Container>
     </Fragment>
   );
