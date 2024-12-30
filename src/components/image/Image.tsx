090e2a54cda7ca0ab7f5c10b09a00201521ac157
Kareem Morsy <kreem.morsy@hotmail.com>
2024-12-31 01:21:26 +0200

    feat: integrate with vehicles api + add Image component with improved error handling

---
+++ src/components/image/Image.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  fallback?: React.ReactNode;
+}
+
+const Image: React.FC<ImageProps> = ({ fallback, title, onError, ...restProps }) => {
+  const [isError, setIsError] = React.useState(false);
+
+  const handleError = (event: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    onError ? onError(event) : setIsError(true);
+  };
+
+  return (() => {
+    if (isError) {
+      if (fallback) {
+        return <>{fallback}</>;
+      }
+      if (title) {
+        return (
+          <div className="uppercase w-full h-full flex items-center justify-center bg-coal-600 text-white p-3 rounded-full">
+            {title[0]}
+          </div>
+        );
+      }
+      return null;
+    }
+
+    return <img {...restProps} title={title} onError={handleError} />;
+  })();
+};
+
+export default Image;
