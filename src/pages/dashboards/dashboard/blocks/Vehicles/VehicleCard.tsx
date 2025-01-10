68c6bdae897e7666671ae9d87b27f5484f3de704
Kareem Morsy <kreem.morsy@hotmail.com>
2025-01-10 22:34:54 +0200

    Refactor vehicle listing by moving vehicles card into its own component and implementing infinite scrolling

---
+++ src/pages/dashboards/dashboard/blocks/Vehicles/VehicleCard.tsx
@@ -0,0 +1,94 @@
+import { VehicleDetails } from '@/api/cars';
+import { StatusDropdown } from '../StatusDropdown';
+import { CarPlate } from '../CarPlate';
+import { toAbsoluteUrl } from '@/utils';
+
+export default function VehicleCard({ vehicle }: { vehicle?: VehicleDetails }) {
+  if (!vehicle) {
+    return (
+      <div className="flex hover:shadow-md h-full w-full flex-col flex-shrink-0 rounded-2xl border border-[#E7E8ED] dark:border-gray-200 overflow-hidden">
+        <div className="h-1 w-full" style={{ backgroundColor: '#212121' }} />
+      </div>
+    );
+  }
+  return (
+    <div className=" hover:shadow-md card h-full w-full flex flex-col flex-shrink-0 rounded-2xl border border-[#E7E8ED] dark:border-gray-200">
+      <div className="flex flex-col grow gap-5 px-8 py-6">
+        <div className="flex justify-between items-center">
+          <CarPlate plate={vehicle.vehicle.plate} />
+          <StatusDropdown
+            selected={vehicle.status}
+            setSelected={() => {}}
+            options={{
+              Unavailable: {
+                color: '#F1416C',
+                backgroundColor: '#FFF5F8'
+              },
+              Maintenance: {
+                color: '#FFA800',
+                backgroundColor: '#FFF8EA'
+              },
+              Available: {
+                color: '#50CD89',
+                backgroundColor: '#EEFAF4'
+              }
+            }}
+          />
+        </div>
+        <div className="flex gap-[38px] justify-between">
+          <div className="flex gap-4">
+            <div className="flex gap-2 items-center">
+              <img
+                src={
+                  vehicle.customer.avatar || toAbsoluteUrl('/media/avatars/avatar-placeholder.png')
+                }
+                className="size-12 rounded-[4px]"
+              />
+              <div className="w-48 text-nowrap">
+                <div className="text-[#3F4254] dark:text-gray-50 font-bold text-[15px] text-ellipsis overflow-hidden">
+                  {vehicle.customer.name}
+                </div>
+                <div className="text-[#B5B5C3] font-medium text-2sm text-ellipsis overflow-hidden">
+                  {vehicle.customer.email}
+                </div>
+              </div>
+            </div>
+          </div>
+          <img
+            src={vehicle.vehicle.brandImage}
+            className="size-9 aspect-square object-cover mr-0.5"
+          />
+        </div>
+        <div className="flex justify-between items-end gap-4">
+          <div className="flex gap-[18px] text-[#72767C] font-dm-sans">
+            <div className="flex gap-1">
+              <img src={toAbsoluteUrl('/media/icons/gauge.svg')} />
+              <span>{vehicle.mileage} KM</span>
+            </div>
+            <div className="flex gap-1">
+              <img src={toAbsoluteUrl('/media/icons/manual.svg')} />
+              <span>{vehicle.type}</span>
+            </div>
+          </div>
+          <div className="text-xs font-semibold text-[#3F4254] max-w-[92px] text-ellipsis overflow-hidden text-nowrap">
+            {vehicle.brandName}
+          </div>
+        </div>
+      </div>
+      <div className="text-xs border-t flex justify-center">
+        <a href="#" className="px-5 py-2 flex gap-2">
+          <img src={toAbsoluteUrl('/media/icons/view-light.svg')} />
+          <span>View</span>
+        </a>
+        <a href="#" className="px-5 py-2 border-x flex gap-2">
+          <img src={toAbsoluteUrl('/media/icons/edit-light.svg')} />
+          <span>Edit</span>
+        </a>
+        <a href="#" className="px-5 py-2 flex gap-2">
+          <img src={toAbsoluteUrl('/media/icons/delete-light.svg')} />
+          <span>Delete</span>
+        </a>
+      </div>
+    </div>
+  );
+}
---
+++ src/pages/dashboards/dashboard/blocks/Vehicles/index.tsx
@@ -0,0 +1,89 @@
+import { getVehicles, VehicleDetails } from '@/api/cars';
+import { Paginated } from '@/api/common';
+import { toAbsoluteUrl } from '@/utils';
+import { useEffect, useMemo, useState } from 'react';
+import VehicleCard from './VehicleCard';
+import { AutoSizer, Grid, InfiniteLoader } from 'react-virtualized';
+
+export function VehicleList() {
+  const [vehicles, setVehicles] = useState<Paginated<VehicleDetails>>();
+  const remoteRowCount = useMemo(() => vehicles?.totalCount ?? 0, [vehicles]);
+  const [offset, setOffset] = useState({ start: 0, end: 10 });
+
+  const isRowLoaded = ({ index }: { index: number }) => !!vehicles?.data[index];
+  const loadMoreRows = async ({
+    startIndex,
+    stopIndex
+  }: {
+    startIndex: number;
+    stopIndex: number;
+  }) => {
+    const drivers = await getVehicles({ start: startIndex, end: stopIndex });
+    setVehicles((prev) => ({
+      data: [...(prev?.data ?? []), ...drivers.data],
+      totalCount: drivers.totalCount
+    }));
+    setOffset({ start: startIndex, end: stopIndex });
+  };
+
+  useEffect(() => {
+    getVehicles({ start: 0, end: 10 }).then(setVehicles);
+  }, []);
+
+  return (
+    <div className="card">
+      <div className="px-7 pt-6 flex items-center justify-between">
+        <div className="card-title">
+          <h3>Vehicle</h3>
+          <h4 className="text-sm font-thin text-[#B5B5C3]">
+            You have {vehicles?.totalCount}{' '}
+            {(vehicles?.totalCount ?? 0 > 1) ? 'vehicles' : 'vehicle'}
+          </h4>
+        </div>
+        <button className="btn btn-info px-8">
+          <img src={toAbsoluteUrl('/media/icons/add-user.svg')} />
+          Add Car
+        </button>
+      </div>
+
+      <div className="card-body pt-2 px-6 pb-3">
+        <AutoSizer disableHeight>
+          {({ width }) => (
+            <InfiniteLoader
+              isRowLoaded={isRowLoaded}
+              loadMoreRows={loadMoreRows}
+              rowCount={remoteRowCount}
+            >
+              {({ onRowsRendered, registerChild }) => (
+                <Grid
+                  ref={registerChild}
+                  className="scrollable-x !overflow-y-hidden"
+                  height={291}
+                  width={width}
+                  columnCount={vehicles?.data.length ?? 0}
+                  columnWidth={402}
+                  rowCount={1}
+                  rowHeight={271}
+                  overscanColumnCount={20}
+                  onSectionRendered={({ columnOverscanStartIndex, columnOverscanStopIndex }) =>
+                    onRowsRendered({
+                      startIndex: columnOverscanStartIndex,
+                      stopIndex: columnOverscanStopIndex
+                    })
+                  }
+                  cellRenderer={({ key, columnIndex: index, style }) =>
+                    vehicles && (
+                      <div key={key} style={style} className="pr-4">
+                        <VehicleCard vehicle={vehicles.data[index]} />
+                      </div>
+                    )
+                  }
+                />
+              )}
+            </InfiniteLoader>
+          )}
+        </AutoSizer>
+      </div>
+    </div>
+  );
+}
