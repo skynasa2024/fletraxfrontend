5442efe42a81ff7a5209131593aca66fb883afb9
Kareem Morsy <kreem.morsy@hotmail.com>
2025-01-17 18:12:42 +0000

    Cleanup app routing setup

--- src/routing/AppRoutingSetup.tsx
+++ src/routing/AppRoutingSetup.tsx
@@ -1,97 +1,23 @@
 import { ReactElement } from 'react';
 import { Navigate, Route, Routes } from 'react-router';
 import { DefaultPage } from '@/pages/dashboards';
-import {
-  ProfileActivityPage,
-  ProfileBloggerPage,
-  CampaignsCardPage,
-  CampaignsListPage,
-  ProjectColumn2Page,
-  ProjectColumn3Page,
-  ProfileCompanyPage,
-  ProfileCreatorPage,
-  ProfileCRMPage,
-  Device,
-  ProfileEmptyPage,
-  ProfileFeedsPage,
-  ProfileGamerPage,
-  ProfileModalPage,
-  ProfileNetworkPage,
-  ProfileNFTPage,
-  ProfilePlainPage,
-  ProfileTeamsPage,
-  ProfileWorksPage
-} from '@/pages/public-profile';
+import { Device } from '@/pages/public-profile';
 import { User } from '@/pages/public-profile/profiles/user';
 import { Vehicles } from '@/pages/vehicle';
 
 import { Driver } from '@/pages/public-profile/profiles/driver';
 import { Maintenance } from '@/pages/public-profile/profiles/maintenance';
 
-import {
-  AccountActivityPage,
-  AccountAllowedIPAddressesPage,
-  AccountApiKeysPage,
-  AccountAppearancePage,
-  AccountBackupAndRecoveryPage,
-  AccountBasicPage,
-  AccountCompanyProfilePage,
-  AccountCurrentSessionsPage,
-  AccountDeviceManagementPage,
-  AccountEnterprisePage,
-  AccountGetStartedPage,
-  AccountHistoryPage,
-  AccountImportMembersPage,
-  AccountIntegrationsPage,
-  AccountInviteAFriendPage,
-  AccountMembersStarterPage,
-  AccountNotificationsPage,
-  AccountOverviewPage,
-  AccountPermissionsCheckPage,
-  AccountPermissionsTogglePage,
-  AccountPlansPage,
-  AccountPrivacySettingsPage,
-  AccountRolesPage,
-  AccountSecurityGetStartedPage,
-  AccountSecurityLogPage,
-  AccountSettingsEnterprisePage,
-  AccountSettingsModalPage,
-  AccountSettingsPlainPage,
-  AccountSettingsSidebarPage,
-  AccountTeamInfoPage,
-  AccountTeamMembersPage,
-  AccountTeamsPage,
-  AccountTeamsStarterPage,
-  AccountUserProfilePage
-} from '@/pages/account';
+import { AccountSettingsPlainPage } from '@/pages/account';
 import { AddUser } from '@/pages/account/home/add-user';
 import { AddDriver } from '@/pages/account/home/add-driver';
 import { AddVehicle } from '@/pages/account/home/add-vehicle';
 import DeviceDetailsPage from '@/pages/public-profile/profiles/default/DeviceDetailsPage';
-import {
-  NetworkAppRosterPage,
-  NetworkMarketAuthorsPage,
-  NetworkAuthorPage,
-  NetworkGetStartedPage,
-  NetworkMiniCardsPage,
-  NetworkNFTPage,
-  NetworkSocialPage,
-  NetworkUserCardsTeamCrewPage,
-  NetworkSaasUsersPage,
-  NetworkStoreClientsPage,
-  NetworkUserTableTeamCrewPage,
-  NetworkVisitorsPage
-} from '@/pages/network';
 
 import { AuthPage } from '@/auth';
 import { RequireAuth } from '@/auth/RequireAuth';
 import { Demo1Layout } from '@/layouts/demo1';
 import { ErrorsRouting } from '@/errors';
-import {
-  AuthenticationWelcomeMessagePage,
-  AuthenticationAccountDeactivatedPage,
-  AuthenticationGetStartedPage
-} from '@/pages/authentication';
 import { MonitoringPage } from '@/pages/monitoring/MonitoringPage';
 import UserDetailsPage from '@/pages/public-profile/profiles/user/blocks/UserDetailsPage';
 import VehicleDetailsPage from '@/pages/vehicle/blocks/VehicleDetailsPage';
@@ -111,22 +37,9 @@ const AppRoutingSetup = (): ReactElement => {
           <Route path="/trips" element={<TripsPage />} />
           <Route path="/geofences" element={<GeofencePage />} />
 
-          <Route path="/users/user/*" element={<User />} />
-          <Route path="/users/add-users" element={<AddUser />} />
-          <Route path="/users/user/view-user" element={<UserDetailsPage />} />
-
-          <Route path="/users/add-user" element={<AddUser />} />
-          <Route path="/drivers/driver" element={<Driver />} />
-          <Route path="/drivers/driver/view-driver" element={<DriverDetailsPage />} />
-          <Route path="/drivers/add-driver" element={<AddDriver />} />
-
           <Route path="/vehicles/vehicle" element={<Vehicles />} />
           <Route path="/vehicles/vehicle/:id" element={<VehicleDetailsPage />} />
           <Route path="/vehicles/add-vehicle" element={<AddVehicle />} />
-
-          <Route path="/devices/device" element={<Device />} />
-          <Route path="/devices/device/view-device" element={<DeviceDetailsPage />} />
-          <Route path="/devices/add-device" element={<AccountSettingsPlainPage />} />
 
           <Route path="/maintenance/maintenance" element={<Maintenance />} />
           <Route
@@ -135,100 +48,18 @@ const AppRoutingSetup = (): ReactElement => {
           />
           <Route path="/maintenance/add-maintenance" element={<AddMaintenance />} />
 
-          <Route path="/public-profile/profiles/creator" element={<ProfileCreatorPage />} />
-          <Route path="/public-profile/profiles/company" element={<ProfileCompanyPage />} />
-          <Route path="/public-profile/profiles/nft" element={<ProfileNFTPage />} />
-          <Route path="/public-profile/profiles/blogger" element={<ProfileBloggerPage />} />
-          <Route path="/public-profile/profiles/crm" element={<ProfileCRMPage />} />
-          <Route path="/public-profile/profiles/gamer" element={<ProfileGamerPage />} />
-          <Route path="/public-profile/profiles/feeds" element={<ProfileFeedsPage />} />
-          <Route path="/public-profile/profiles/plain" element={<ProfilePlainPage />} />
-          <Route path="/public-profile/profiles/modal" element={<ProfileModalPage />} />
-          <Route path="/public-profile/projects/3-columns" element={<ProjectColumn3Page />} />
-          <Route path="/public-profile/projects/2-columns" element={<ProjectColumn2Page />} />
-          <Route path="/public-profile/works" element={<ProfileWorksPage />} />
-          <Route path="/public-profile/teams" element={<ProfileTeamsPage />} />
-          <Route path="/public-profile/network" element={<ProfileNetworkPage />} />
-          <Route path="/public-profile/activity" element={<ProfileActivityPage />} />
-          <Route path="/public-profile/campaigns/card" element={<CampaignsCardPage />} />
-          <Route path="/public-profile/campaigns/list" element={<CampaignsListPage />} />
-          <Route path="/public-profile/empty" element={<ProfileEmptyPage />} />
-          <Route path="/account/home/get-started" element={<AccountGetStartedPage />} />
-          <Route path="/account/home/user-profile" element={<AccountUserProfilePage />} />
-          <Route path="/account/home/company-profile" element={<AccountCompanyProfilePage />} />
-          <Route path="/account/home/settings-sidebar" element={<AccountSettingsSidebarPage />} />
-          <Route
-            path="/account/home/settings-enterprise"
-            element={<AccountSettingsEnterprisePage />}
-          />
+          <Route path="/devices/device" element={<Device />} />
+          <Route path="/devices/device/view-device" element={<DeviceDetailsPage />} />
+          <Route path="/devices/add-device" element={<AccountSettingsPlainPage />} />
 
-          <Route path="/account/home/settings-modal" element={<AccountSettingsModalPage />} />
-          <Route path="/account/billing/basic" element={<AccountBasicPage />} />
-          <Route path="/account/billing/enterprise" element={<AccountEnterprisePage />} />
-          <Route path="/account/billing/plans" element={<AccountPlansPage />} />
-          <Route path="/account/billing/history" element={<AccountHistoryPage />} />
-          <Route path="/account/security/get-started" element={<AccountSecurityGetStartedPage />} />
-          <Route path="/account/security/overview" element={<AccountOverviewPage />} />
-          <Route
-            path="/account/security/allowed-ip-addresses"
-            element={<AccountAllowedIPAddressesPage />}
-          />
-          <Route
-            path="/account/security/privacy-settings"
-            element={<AccountPrivacySettingsPage />}
-          />
-          <Route
-            path="/account/security/device-management"
-            element={<AccountDeviceManagementPage />}
-          />
-          <Route
-            path="/account/security/backup-and-recovery"
-            element={<AccountBackupAndRecoveryPage />}
-          />
-          <Route
-            path="/account/security/current-sessions"
-            element={<AccountCurrentSessionsPage />}
-          />
-          <Route path="/account/security/security-log" element={<AccountSecurityLogPage />} />
-          <Route path="/account/members/team-starter" element={<AccountTeamsStarterPage />} />
-          <Route path="/account/members/teams" element={<AccountTeamsPage />} />
-          <Route path="/account/members/team-info" element={<AccountTeamInfoPage />} />
-          <Route path="/account/members/members-starter" element={<AccountMembersStarterPage />} />
-          <Route path="/account/members/team-members" element={<AccountTeamMembersPage />} />
-          <Route path="/account/members/import-members" element={<AccountImportMembersPage />} />
-          <Route path="/account/members/roles" element={<AccountRolesPage />} />
-          <Route
-            path="/account/members/permissions-toggle"
-            element={<AccountPermissionsTogglePage />}
-          />
-          <Route
-            path="/account/members/permissions-check"
-            element={<AccountPermissionsCheckPage />}
-          />
-          <Route path="/account/integrations" element={<AccountIntegrationsPage />} />
-          <Route path="/account/notifications" element={<AccountNotificationsPage />} />
-          <Route path="/account/api-keys" element={<AccountApiKeysPage />} />
-          <Route path="/account/appearance" element={<AccountAppearancePage />} />
-          <Route path="/account/invite-a-friend" element={<AccountInviteAFriendPage />} />
-          <Route path="/account/activity" element={<AccountActivityPage />} />
-          <Route path="/network/get-started" element={<NetworkGetStartedPage />} />
-          <Route path="/network/user-cards/mini-cards" element={<NetworkMiniCardsPage />} />
-          <Route path="/network/user-cards/team-crew" element={<NetworkUserCardsTeamCrewPage />} />
-          <Route path="/network/user-cards/author" element={<NetworkAuthorPage />} />
-          <Route path="/network/user-cards/nft" element={<NetworkNFTPage />} />
-          <Route path="/network/user-cards/social" element={<NetworkSocialPage />} />
-          <Route path="/network/user-table/team-crew" element={<NetworkUserTableTeamCrewPage />} />
-          <Route path="/network/user-table/app-roster" element={<NetworkAppRosterPage />} />
-          <Route path="/network/user-table/market-authors" element={<NetworkMarketAuthorsPage />} />
-          <Route path="/network/user-table/saas-users" element={<NetworkSaasUsersPage />} />
-          <Route path="/network/user-table/store-clients" element={<NetworkStoreClientsPage />} />
-          <Route path="/network/user-table/visitors" element={<NetworkVisitorsPage />} />
-          <Route path="/auth/welcome-message" element={<AuthenticationWelcomeMessagePage />} />
-          <Route
-            path="/auth/account-deactivated"
-            element={<AuthenticationAccountDeactivatedPage />}
-          />
-          <Route path="/authentication/get-started" element={<AuthenticationGetStartedPage />} />
+          <Route path="/users/user/*" element={<User />} />
+          <Route path="/users/add-users" element={<AddUser />} />
+          <Route path="/users/user/view-user" element={<UserDetailsPage />} />
+          <Route path="/users/add-user" element={<AddUser />} />
+
+          <Route path="/drivers/driver" element={<Driver />} />
+          <Route path="/drivers/driver/view-driver" element={<DriverDetailsPage />} />
+          <Route path="/drivers/add-driver" element={<AddDriver />} />
         </Route>
       </Route>
       <Route path="error/*" element={<ErrorsRouting />} />
