4cb96ad013aace9ff404d699d9268ebf0906b634
Kareem Morsy <kreem.morsy@hotmail.com>
2025-01-17 17:47:43 +0000

    Fix maintenance and violations

--- src/pages/dashboards/dashboard/blocks/CarView.tsx
+++ src/pages/dashboards/dashboard/blocks/CarView.tsx
@@ -1,5 +1,8 @@
 import { Vehicle } from '@/api/cars';
 import { CarPlate } from '@/pages/dashboards/dashboard/blocks/CarPlate';
+import Image from '@/components/image/Image';
+import { toAbsoluteUrl } from '@/utils';
+import { KeenIcon } from '@/components';
 
 export interface CarViewProps {
   vehicle: Vehicle;
@@ -9,7 +12,17 @@ export const CarView = ({ vehicle }: CarViewProps) => {
   return (
     <div className="flex flex-col gap-1 px-2">
       <div className="flex gap-2 items-center">
-        <img src={vehicle.brandImage} className="size-9 object-cover" />
+        <Image
+          src={vehicle.brandImage}
+          alt={vehicle.name}
+          title={vehicle.name}
+          className="size-9 object-cover aspect-square"
+          fallback={
+            <div className="bg-neutral-200 size-9 aspect-square rounded-full flex items-center justify-center">
+              <KeenIcon style="duotone" icon="car" className="text-black" />
+            </div>
+          }
+        />
         <CarPlate plate={vehicle.plate} />
       </div>
       <div className="text-gray-600 text-2sm">{vehicle.imei}</div>
