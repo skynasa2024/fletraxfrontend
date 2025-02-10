76ece1c199599fd8a205102a224166fa91e1034e
Ahmed Samy <ahmedsamy4664@gmail.com>
2025-02-10 07:47:46 +0200

    Move discard button beside the save button

--- src/pages/vehicle/add-vehicle/AddVehiclePage.tsx
+++ src/pages/vehicle/add-vehicle/AddVehiclePage.tsx
@@ -436,7 +436,14 @@ const AddVehiclePage = () => {
                       </div>
                     </div>
 
-                    <div className="mt-5 flex justify-end gap-4">
+                    <div className="mt-5 flex justify-end items-center gap-4">
+                      <button
+                        onClick={() => navigate(-1)}
+                        type="button"
+                        className="text-red-700 hover:text-white border bg-red-100 border-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-xs px-5 py-2.5 text-center me-2 dark:border-red-500 dark:text-red-500 dark:hover:text-white dark:hover:bg-red-600 dark:focus:ring-red-900"
+                      >
+                        Discard
+                      </button>
                       <button
                         className={clsx(
                           'px-4 py-2 bg-success text-white rounded-lg transition-all duration-300 ease-in-out flex items-center justify-center gap-3',
