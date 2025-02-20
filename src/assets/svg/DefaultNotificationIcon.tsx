ea58019f7dc1fee68ce993a1d95a9f7c626583fd
Ahmed Samy <ahmedsamy4664@gmail.com>
2025-02-21 00:19:32 +0200

    Add new notification icons and update SVG index

---
+++ src/assets/svg/DefaultNotificationIcon.tsx
@@ -0,0 +1,38 @@
+export default function DefaultNotificationIcon() {
+  return (
+    <svg width="18" height="20" viewBox="0 0 18 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M5.35179 18.2418C6.19288 19.311 7.51418 20 9 20C10.4858 20 11.8071 19.311 12.6482 18.2418C10.2264 18.57 7.77357 18.57 5.35179 18.2418Z"
+        fill="url(#paint0_linear_4611_63655)"
+      />
+      <path
+        d="M15.7491 7V7.7041C15.7491 8.54909 15.9903 9.37517 16.4422 10.0782L17.5496 11.8012C18.5612 13.3749 17.789 15.5139 16.0296 16.0116C11.4273 17.3134 6.57274 17.3134 1.97036 16.0116C0.211046 15.5139 -0.561177 13.3749 0.450359 11.8012L1.5578 10.0782C2.00972 9.37517 2.25087 8.54909 2.25087 7.7041V7C2.25087 3.13401 5.27256 0 9 0C12.7274 0 15.7491 3.13401 15.7491 7Z"
+        fill="url(#paint1_linear_4611_63655)"
+      />
+      <defs>
+        <linearGradient
+          id="paint0_linear_4611_63655"
+          x1="9"
+          y1="0"
+          x2="9"
+          y2="20"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#5271FF" />
+          <stop offset="1" stop-color="#5271FF" stop-opacity="0.45" />
+        </linearGradient>
+        <linearGradient
+          id="paint1_linear_4611_63655"
+          x1="9"
+          y1="0"
+          x2="9"
+          y2="20"
+          gradientUnits="userSpaceOnUse"
+        >
+          <stop stop-color="#5271FF" />
+          <stop offset="1" stop-color="#5271FF" stop-opacity="0.45" />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
